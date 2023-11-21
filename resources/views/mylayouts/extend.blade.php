268b4053ddb01a70490e1ea4620ab92fa56d6b16
muneeber <muneeber358@gmail.com>
2023-11-21 13:01:05 +0500

    back

--- resources/views/mylayouts/extend.blade.php
+++ resources/views/mylayouts/extend.blade.php
@@ -77,7 +77,7 @@
                     </li>
                     <li class="px-5">
                         <div class="flex flex-row items-center h-8">
-                            <div class="text-sm font-light tracking-wide text-gray-500">pots</div>
+                            <div class="text-sm font-light tracking-wide text-gray-500">Reports</div>
                         </div>
                     </li>
 
